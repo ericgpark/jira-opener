8fc54056d38550dcd445df126f98d6b3bccd9576
ericgpark <ericparkmusic@gmail.com>
2025-06-17 14:32:16 -0700

    Remove test repository

--- jira-opener-test/src/sample.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const helloWorld = () => {
-    // To-do: Add functionality to define how many times to log 'hello world'. To be covered in CRM-3
-    console.log('hello world!');
-};
-//# sourceMappingURL=sample.js.map
--- jira-opener-test/src/sample.ts
+++
@@ -1,4 +0,0 @@
-const helloWorld = () => {
-  // To-do: Add functionality to define how many times to log 'hello world'. To be covered in CRM-3
-  console.log('hello world!');
-}
